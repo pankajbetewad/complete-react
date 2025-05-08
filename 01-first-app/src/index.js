5bd5db803f467d9fb33c4b8b717e03f70b23ce14
pankaj betewad <pankajbetewad@gmail.com>
2025-05-08 07:08:18 +0000

    formatted code

--- 01-first-app/src/index.js
+++ 01-first-app/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const FirstComponent = () => {
-    return <>
-        <h1>Hello, this is my first component</h1>
-    </>
+    return (
+        <>
+            <h1>Hello, this is my first component</h1>
+        </>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
