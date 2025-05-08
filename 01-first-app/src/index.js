fa62468ddbfb40a9b0867abbf057bf7b7c906be0
pankaj betewad <pankajbetewad@gmail.com>
2025-05-08 07:06:14 +0000

    Added first app

---
+++ 01-first-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const FirstComponent = () => {
+    return <>
+        <h1>Hello, this is my first component</h1>
+    </>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<FirstComponent />)
